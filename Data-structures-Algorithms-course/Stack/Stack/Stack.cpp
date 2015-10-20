8489e5eecebdf8e5066e440e30f62d8c203b7455
Petar Ivanov <pepincho95@gmail.com>
2015-10-20 20:43:57 +0300

    add function definition in Stack.cpp

--- Data-structures-Algorithms-course/Stack/Stack/Stack.cpp
+++ Data-structures-Algorithms-course/Stack/Stack/Stack.cpp
@@ -1,9 +1,76 @@
 #include "Stack.h"
 
+template <typename T>
+Stack<T>::Node::Node(T data, Node* pNext) {
 
-Stack::Stack() {
 }
 
+template <typename T>
+Stack<T>::Stack() {
 
-Stack::~Stack() {
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+
+}
+
+template <typename T>
+Stack<T>::Stack(Stack const & other) {
+
+}
+
+template <typename T>
+Stack<T>& Stack<T>::operator=(Stack const & other) {
+
+}
+
+template <typename T>
+void Stack<T>::init() {
+
+}
+
+template <typename T>
+void Stack<T>::destroy() {
+
+}
+
+template <typename T>
+void Stack<T>::copyFrom(Stack const & other) {
+
+}
+
+template <typename T>
+bool Stack<T>::push(T element) {
+
+}
+
+template <typename T>
+bool Stack<T>::pop(T& element) {
+
+}
+
+template <typename T>
+T Stack<T>::peek() const {
+
+}
+
+template <typename T>
+void Stack<T>::removeAll() {
+
+}
+
+template <typename T>
+size_t Stack<T>::getAllocatedSize() const {
+
+}
+
+template <typename T>
+size_t Stack<T>::getSize() const {
+
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() const {
+
 }
--- Data-structures-Algorithms-course/Stack/Stack/main.cpp
+++ Data-structures-Algorithms-course/Stack/Stack/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "Stack.h"
+
 using namespace std;
 
 int main() {
