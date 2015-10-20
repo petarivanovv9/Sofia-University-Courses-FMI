9e35dd55ad061bf1338c67bf621f4a8409fc58d0
Petar Ivanov <pepincho95@gmail.com>
2015-10-20 21:20:19 +0300

    add part of the function declarations

--- Data-structures-Algorithms-course/Stack/Stack/Stack.cpp
+++ Data-structures-Algorithms-course/Stack/Stack/Stack.cpp
@@ -1,43 +1,85 @@
+#include <exception>
+
 #include "Stack.h"
 
 template <typename T>
-Stack<T>::Node::Node(T data, Node* pNext) {
-
+Stack<T>::Node::Node(T _data, Node* _pNext) {
+	this->data = _data;
+	this->pNext = _pNext;
 }
 
 template <typename T>
 Stack<T>::Stack() {
-
+	init();
 }
 
 template <typename T>
 Stack<T>::~Stack() {
-
+	destroy();
 }
 
 template <typename T>
 Stack<T>::Stack(Stack const & other) {
-
+	init();
+	copyFrom();
 }
 
 template <typename T>
 Stack<T>& Stack<T>::operator=(Stack const & other) {
-
+	if (this != &other) {
+		destroy();
+		copyFrom;
+	}
+	
+	return *this;
 }
 
 template <typename T>
 void Stack<T>::init() {
-
+	this->pTop = 0;
+	this->used = 0;
 }
 
 template <typename T>
 void Stack<T>::destroy() {
+	Node* p;
 
+	while (pTop) {
+		p = pTop;
+		pTop = pTop->pNext;
+		delete p;
+	}
+
+	init();
 }
 
 template <typename T>
 void Stack<T>::copyFrom(Stack const & other) {
+	if (other.isEmpty()) {
+		return;
+	}
 
+	Node *ours, *theirs;
+
+	try {
+		pTop = new Node(other.pTop->data);
+
+		ours = pTop;
+		theirs = other.pTop->pNext;
+
+		while (theirs) {
+			ours->pNext = new Node(theirs->data);
+			ours = ours->pNext;
+			theirs = theirs->pNext;
+		}
+		
+		used = other.used;
+	}
+	catch (std::bad_alloc&) {
+		destroy();
+
+		throw();
+	}
 }
 
 template <typename T>
